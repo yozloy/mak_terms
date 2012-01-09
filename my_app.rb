1daa92818077ec7db63de114a6a5b6c488d6cb49
yozloy <yozloy@me.com>
2012-01-09 13:01:52 +0800

    form search

--- my_app.rb
+++ my_app.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
 require 'mongo'
 class MyApp < Sinatra::Base
-	get '/' do
-		"Today is good"
+	get '/search' do
+<<HERE
+  <form action="search" method="post">
+	<input type="text" name="name[word]" />
+	<input type="submit" name="get the Chinese" />
+	</form>
+HERE
+	end
+	post '/search' do
+	@word = params[:name][:word]
+	redirect to ("/terms/#{@word}")
 	end
 
-	get '/:name' do |n|
+	get '/terms/:name' do |n|
 	db = Mongo::Connection.new('mongoc2.grandcloud.cn',10003).db('terms')
 	db.authenticate('yozloy','0054444944')
 	coll = db.collection('drum')
